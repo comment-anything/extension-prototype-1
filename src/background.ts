3821c8724a0bf921094598074af1ab415487857e
Karl Miller <karlmiller127@gmail.com>
2022-09-23 12:47:55 -0400

    popup to background comms still not working

--- src/background.ts
+++ src/background.ts
@@ -1,4 +1,8 @@
-
-
 console.log("background script running")
 
+
+browser.runtime.onMessage.addListener((message)=>{
+    console.log("message received", message)
+})
+
+browser.runtime.sendMessage("hello")
--- src/popup.ts
+++ src/popup.ts
@@ -1,4 +1,5 @@
 import { UrlDisplay } from "./UrlDisplay";
+import { util } from "./utility";
 
 
 
